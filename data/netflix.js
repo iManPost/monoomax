261e4b4e4d7865eca84346edacd210addcd5eabd
iManPost <iveenvon@gmail.com>
2026-07-01 08:42:31 +0700

    Update netflix.js

--- data/netflix.js
+++ data/netflix.js
@@ -1,109 +1,123 @@
 export const netflixMovies = [
+    
+    {
+    name: 'คนเป็นฝ่านรกซอมบี้',
+    group: '🎬netflix2026',
+    logo: 'https://www.nunghd4k.com/wp-content/uploads/2023/11/Alive-Saraitda-2020.jpg',
+    url: 'https://api.p2p-cdnmovie.xyz/1080p/dvhz8i/dvhz8i/index.m3u8'
+  },
+   {
+    name: 'อธิษฐานคริสต์มาส',
+    group: '🎬netflix2026',
+    logo: 'https://9.9nung.com/wp-content/uploads/2026/06/36BnxmIvWvOsMmlQfbI0ilTfuvh-185x278.jpg',
+    url: 'https://api.p2p-cdnmovie.xyz/1080p/3w0rva/3w0rva/index.m3u8'
+  },
+   
    {
     name: 'Teach You a Lesson (2026) ตอนที่ 1',
-    group: '🎬ซีรีย์2026',
+    group: '🎬netflix2026',
     logo: 'https://9.9nung.com/wp-content/uploads/2026/06/36BnxmIvWvOsMmlQfbI0ilTfuvh-185x278.jpg',
     url: 'https://media.vdohls.com/s3zOEFC_O-L2e/video.m3u8'
   },
   {
     name: 'Teach You a Lesson (2026) ตอนที่  2',
-    group: '🎬ซีรีย์2026',
+    group: '🎬netflix2026',
     logo: 'https://9.9nung.com/wp-content/uploads/2026/06/36BnxmIvWvOsMmlQfbI0ilTfuvh-185x278.jpg',
     url: 'https://media.vdohls.com/2Fa9_1DgQ-0AX/video.m3u8'
   },
   {
     name: 'Teach You a Lesson (2026) ตอนที่  3',
-    group: '🎬ซีรีย์2026',
+    group: '🎬netflix2026',
     logo: 'https://9.9nung.com/wp-content/uploads/2026/06/36BnxmIvWvOsMmlQfbI0ilTfuvh-185x278.jpg',
     url: 'https://media.vdohls.com/K_ob5K56-VAFc/video.m3u8'
   },
   {
     name: 'Teach You a Lesson (2026) ตอนที่  4',
-    group: '🎬ซีรีย์2026',
+    group: '🎬netflix2026',
     logo: 'https://9.9nung.com/wp-content/uploads/2026/06/36BnxmIvWvOsMmlQfbI0ilTfuvh-185x278.jpg',
     url: 'https://media.vdohls.com/2Tqp-Q_qBUfoH/video.m3u8'
   },
   {
     name: 'Teach You a Lesson (2026) ตอนที่ 5',
-    group: '🎬ซีรีย์2026',
+    group: '🎬netflix2026',
     logo: 'https://9.9nung.com/wp-content/uploads/2026/06/36BnxmIvWvOsMmlQfbI0ilTfuvh-185x278.jpg',
     url: 'https://media.vdohls.com/tOzGrIyb-p_UU/video.m3u8'
   },
   {
     name: 'Teach You a Lesson (2026) ตอนที่ 6',
-    group: '🎬ซีรีย์2026',
+    group: '🎬netflix2026',
     logo: 'https://9.9nung.com/wp-content/uploads/2026/06/36BnxmIvWvOsMmlQfbI0ilTfuvh-185x278.jpg',
     url: 'https://media.vdohls.com/SSU-ty3USC_vN/video.m3u8'
   },
   {
     name: 'Teach You a Lesson (2026) ตอนที่  7',
-    group: '🎬ซีรีย์2026',
+    group: '🎬netflix2026',
     logo: 'https://9.9nung.com/wp-content/uploads/2026/06/36BnxmIvWvOsMmlQfbI0ilTfuvh-185x278.jpg',
     url: 'https://media.vdohls.com/CejH-Am_xAgTY/video.m3u8'
   },
   {
     name: 'Teach You a Lesson (2026) ตอนที่  8',
-    group: '🎬ซีรีย์2026',
+    group: '🎬netflix2026',
     logo: 'https://9.9nung.com/wp-content/uploads/2026/06/36BnxmIvWvOsMmlQfbI0ilTfuvh-185x278.jpg',
     url: 'https://media.vdohls.com/ABv_5i8-iZYzz/video.m3u8'
   },
   {
     name: 'Teach You a Lesson (2026) ตอนที่ 9',
-    group: '🎬ซีรีย์2026',
+    group: '🎬netflix2026',
     logo: 'https://9.9nung.com/wp-content/uploads/2026/06/36BnxmIvWvOsMmlQfbI0ilTfuvh-185x278.jpg',
     url: 'https://media.vdohls.com/tgvlVVIw-y_1F/video.m3u8'
   },
   {
     name: 'Teach You a Lesson (2026) ตอนที่ 10',
-    group: '🎬ซีรีย์2026',
+    group: '🎬netflix2026',
     logo: 'https://9.9nung.com/wp-content/uploads/2026/06/36BnxmIvWvOsMmlQfbI0ilTfuvh-185x278.jpg',
     url: 'https://media.vdohls.com/bwmXc-PGCh_VT/video.m3u8'
   },
   {
     name: 'ทนายปีศาจ ตอนที่ 1',
-    group: '🎬ซีรีย์2026',
+    group: '🎬netflix2026',
     logo: 'https://media.online-station.net/images/2026/06/d86a402604ec144bdb6df4961fc6caa1.jpg',
     url: 'https://media.vdohls.com/q9oJ-rlNC_ukL/video.m3u8'
   },
   {
     name: 'ทนายปีศาจ ตอนที 2',
-    group: '🎬ซีรีย์2026',
+    group: '🎬netflix2026',
     logo: 'https://media.online-station.net/images/2026/06/d86a402604ec144bdb6df4961fc6caa1.jpg',
     url: 'https://media.vdohls.com/Scc_Hy2-ocrmW/video.m3u8'
   },
   {
     name: 'ทนายปีศาจ ตอนที 3',
-    group: '🎬ซีรีย์2026',
+    group: '🎬netflix2026',
     logo: 'https://media.online-station.net/images/2026/06/d86a402604ec144bdb6df4961fc6caa1.jpg',
     url: 'https://media.vdohls.com/L-XSBI_TfELKH/video.m3u8'
   },
   {
     name: 'ทนายปีศาจ ตอนที 4',
-    group: '🎬ซีรีย์2026',
+    group: '🎬netflix2026',
     logo: 'https://media.online-station.net/images/2026/06/d86a402604ec144bdb6df4961fc6caa1.jpg',
     url: 'https://media.vdohls.com/wSE-KroXm9_K8/video.m3u8'
   },
   {
     name: 'ทนายปีศาจ ตอนที 5',
-    group: '🎬ซีรีย์2026',
+    group: '🎬netflix2026',
     logo: 'https://media.online-station.net/images/2026/06/d86a402604ec144bdb6df4961fc6caa1.jpg',
     url: 'https://media.vdohls.com/Osj-vqP_sz6y9/video.m3u8'
   },
   {
     name: 'ทนายปีศาจ ตอนที 6',
-    group: '🎬ซีรีย์2026',
+    group: '🎬netflix2026',
     logo: 'https://media.online-station.net/images/2026/06/d86a402604ec144bdb6df4961fc6caa1.jpg',
     url: 'https://media.vdohls.com/5-1x_c2dYUbTp/video.m3u8'
   },
   {
     name: 'ทนายปีศาจ ตอนที 7',
-    group: '🎬ซีรีย์2026',
+    group: '🎬netflix2026',
     logo: 'https://media.online-station.net/images/2026/06/d86a402604ec144bdb6df4961fc6caa1.jpg',
     url: 'https://media.vdohls.com/4_G-vTzUdbTaV/video.m3u8'
   },
   {
     name: 'ทนายปีศาจ ตอนที 8',
-    group: '🎬ซีรีย์2026',
+    group: '🎬netflix2026',
     logo: 'https://media.online-station.net/images/2026/06/d86a402604ec144bdb6df4961fc6caa1.jpg',
     url: 'https://media.vdohls.com/gxbm5Jg_YH-ZE/video.m3u8'
   },
