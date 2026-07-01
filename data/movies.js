d05ca91658fbf16ac91d4c27bb4bd78fbc56184f
iManPost <iveenvon@gmail.com>
2026-07-01 07:52:09 +0700

    Update movies.js

--- data/movies.js
+++ data/movies.js
@@ -1,9 +1,15 @@
 export const movies = [
+  
+    { name: 'Apex (2026) ห่วงโซ่สังหาร', group: 'หนังใหม่2026', logo: 'https://moviesdoofree.com/wp-content/uploads/2026/04/1-11.jpg', url: 'https://m3u8haha.com/movie/M15773.mp4/playlist.m3u8' },
+  { name: 'War Machine (2026) สงครามจักรกลถล่มโลก', group: 'หนังใหม่2026', logo: 'https://www.24hd.net/wp-content/uploads/2026/03/war-machine.jpg.webp', url: 'https://media.vdohls.com/axCYYDs_n-62N/playlist.m3u8' },
+  
+  
   {
     name: 'ฝ่าห้วงฝันสู่ 2026',
     group: '🎬หนังใหม่2026',
     logo: 'https://9.9nung.com/wp-content/uploads/2026/06/dKT6A7yQWxvRhZwX5uudHofvpja-185x278.jpg',
     url: 'https://media.vdohls.com/E-JftGY_H5Upq/playlist.m3u8'
+
   },
   {
     name: 'ฝันร้ายกลางทะเล 2026',
