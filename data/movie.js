816ec130af0cb760a8fc92b0638538299441b40a
iManPost <iveenvon@gmail.com>
2026-07-01 07:07:08 +0700

    Update movie.js

--- data/movie.js
+++ data/movie.js
@@ -1,4 +1,4 @@
-export const movies = [
+export const หนังใหม่2026 = [
   { name: 'Apex (2026) ห่วงโซ่สังหาร', group: 'หนังใหม่2026', logo: 'https://moviesdoofree.com/wp-content/uploads/2026/04/1-11.jpg', url: 'https://m3u8haha.com/movie/M15773.mp4/playlist.m3u8' },
   { name: 'War Machine (2026) สงครามจักรกลถล่มโลก', group: 'หนังใหม่2026', logo: 'https://www.24hd.net/wp-content/uploads/2026/03/war-machine.jpg.webp', url: 'https://media.vdohls.com/axCYYDs_n-62N/playlist.m3u8' },
   { name: 'The Last Shot (2025) มือปืน', group: 'หนังใหม่2026', logo: 'https://www.24hd.net/wp-content/uploads/2025/10/The-Last-Shot-2025.webp', url: 'https://media.vdohls.com/A_BUy7n-TkOPS/playlist.m3u8' },
